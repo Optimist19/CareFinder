250c70d7c7a2b2c97703aed4cb16e54240d5e795
optimist19 <oluwasegunbamidele3@gmail.com>
2023-06-28 22:16:39 +0100

    copied the google code for the SEO in the index.html

--- tests/unit/AboutComp.spec.js
+++ tests/unit/AboutComp.spec.js
@@ -4,7 +4,7 @@ import {mount} from '@vue/test-utils'
 describe("AboutComp.vue and HomeComp", ()=>{
 	it('Testing if the first paragraph has the class of first-para', ()=>{
 		let wrapper = mount(AboutComp)
-		console.log(wrapper)
+		// console.log(wrapper)
 		expect(wrapper.find('.first-para').exists()).toBe(true);
 	})
 })
